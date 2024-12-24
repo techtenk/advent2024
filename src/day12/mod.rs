7b170099fa5e2797f27e41cb44a636a57e62d872
techtenk <techtenk@gmail.com>
2024-12-24 10:13:17 -0600

    day 12 mostly working, just have to merge some more plots correctly. still pretty slow

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use crate::utils::grid::Grid;
 
 pub(crate) fn run(test: bool) {
@@ -8,23 +9,22 @@ pub(crate) fn run(test: bool) {
     let map = Grid::from(input);
 
     // group the plots together
-    let mut plots: Vec<Vec<(usize, usize)>> = Vec::new();
+    let mut plots: Vec<HashSet<(usize, usize)>> = Vec::new();
     for row in 0..map.get_height() {
         for col in 0..map.get_width() {
+            // println!("Looking for plots that contain ({}, {}) with plant {}", row, col, map.get(row, col).unwrap());
             // if cell is already in a plot, skip it
             if plots.iter().any(|plot| plot.contains(&(row, col))) {
                 continue;
             }
             let c = map.get(row, col);
             if c.is_some() {
+                let mut partial_plot = HashSet::new();
                 // recursively find all the connected plant spaces
-                let mut partial_plot: Vec<(usize, usize)> = find_plot(row, col, c.unwrap(), &map).unwrap();
+                find_plot(row, col, c.unwrap(), &map, &mut partial_plot);
 
-                // at this point the plot may have duplicates and it may be incomplete
-                partial_plot.sort();
-                partial_plot.dedup();
                 // if any of the cells in this plot are already in another plot, merge them
-                let mut plot_to_merge: Option<&mut Vec<(usize, usize)>> = None;
+                let mut plot_to_merge: Option<&mut HashSet<(usize, usize)>> = None;
                 plots.iter_mut().any(|plot| {
                     if partial_plot.iter().any(|cell| plot.contains(cell)) {
                         plot_to_merge = Some(plot);
@@ -35,8 +35,6 @@ pub(crate) fn run(test: bool) {
                 });
                 if let Some(ptm) = plot_to_merge.as_mut() {
                     ptm.extend(partial_plot);
-                    ptm.sort();
-                    ptm.dedup();
                 } else {
                     // add the plot to the list of plots
                     plots.push(partial_plot);
@@ -65,28 +63,64 @@ pub(crate) fn run(test: bool) {
 
     let mut total_fence_cost = 0u32;
     for p in plots.iter().zip(perimeter_counts.iter()) {
-        println!("{} plot is size: {}, perimeter {}", map.get(p.0[0].0, p.0[0].1).unwrap(), p.0.len(), p.1);
+        let first = p.0.iter().next().unwrap();
+        println!("{} plot is size: {}, perimeter {}", map.get(first.0, first.1).unwrap(), p.0.len(), p.1);
+
+        /** Print the plots to look for ones that should connect but don't
+        print the plots, for every plot we are going to print the whole map and if the point is in the plot, we print it
+        if it is not in the plot but matches the letter, print the letter in lowercase
+        otherwise, print .
+        for row in 0..map.get_height() {
+            for col in 0..map.get_width() {
+                let c = map.get(row, col).unwrap();
+                if p.0.contains(&(row, col)) {
+                    print!("{}", c);
+                } else if c == map.get(first.0, first.1).unwrap() {
+                    print!("{}", c.to_lowercase());
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        */
+
+        // look for plots points that are contained in more than one plot
+        for other_plot in plots.iter().filter(|plot| *plot != p.0) {
+            let intersection: HashSet<_> = p.0.intersection(other_plot).collect();
+            if !intersection.is_empty() {
+                println!("Found intersection of size {} between plots of size {} and {}", intersection.len(), p.0.len(), other_plot.len());
+            }
+        }
+
         // cost is area * perimeter
         total_fence_cost += (p.0.len() as u32) * p.1;
     }
     println!("Total fence cost: {}", total_fence_cost);
 }
 
-fn find_plot(row: usize, col: usize, plant: char, map: &Grid<char>) -> Option<Vec<(usize, usize)>> {
+fn find_plot(row: usize, col: usize, plant: char, map: &Grid<char>, plot_points: &mut HashSet<(usize, usize)>) {
     // base case, the cell does not match
     if map.get(row, col) != Some(plant) {
-        return None
+        return;
     }
-    let mut plot = vec![(row, col)];
+    plot_points.insert((row, col));
     // so we don't have infinite recursion, only check down and right
-    let (_, right, _, down) = map.get_adjacent(row, col);
+    let right = map.get_right(row, col);
+    if right.is_some() {
+        // println!("Checking right: {}", right.unwrap());
+    }
     match right {
-        Some(r) if r == plant => plot.extend(find_plot(row, col + 1, plant, map)?),
+        Some(r) if r == plant => find_plot(row, col + 1, plant, map, plot_points),
         _ => (),
+    }
+    let down = map.get_bottom(row, col);
+    if down.is_some() {
+        // println!("Checking down: {}", down.unwrap());
     }
     match down {
-        Some(d) if d == plant => plot.extend(find_plot(row + 1, col, plant, map)?),
+        Some(d) if d == plant => find_plot(row + 1, col, plant, map, plot_points),
         _ => (),
     }
-    Some(plot)
+
 }
